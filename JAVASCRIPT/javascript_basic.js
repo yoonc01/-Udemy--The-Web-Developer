b3c9e972268a6b080349ae98844dee893c8df604
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-01-05 00:06:55 +0900

    javascript_basic done

---
+++ JAVASCRIPT/javascript_basic.js
@@ -0,0 +1,44 @@
+/*
+    Primitive Types
+*/
+
+// Number (int + float)
+// 따라서 / 도 int 범위에서 끝나지 않음 ** 로 거듭제곱 표현 가능
+// NaN는 Not a Number이지만 숫자로 침(0 / 0 등)
+
+// String
+// Boolean(true or false)
+// use when performing logic
+
+// Null(typeof 시 object 반환 설계 결함으로 null은 기본형이 맞음)
+// Undefined
+
+// Symbol
+// Bigint
+
+/*
+    Variables
+*/
+
+// let (keyword that use to store value in variable)
+
+// Usage >> let year = 2024;
+// Updating variable year = year + 1
+// Updating shortcut(add 1 or sub 1) year++, year--, ++year, --year
+
+// year++, year-- => return value before increment or decrement
+// ++year, --year => return value after increment or decrement
+
+// const (constant)
+// can reassign, update the value
+
+// var(old method)
+
+/*
+    Naming variable
+*/
+
+// cannot start with a digit or digits
+// snake case current_date(uncommon)
+// camel case currentDate(common)
+// (suggest) use is to show that it is a boolean(ex. isGameOver)
