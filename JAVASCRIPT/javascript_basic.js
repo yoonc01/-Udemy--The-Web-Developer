7c7822f08d22c16f487af33de3d23c0e09700129
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-01-05 12:06:20 +0900

    Update javascript_basic.js

--- JAVASCRIPT/javascript_basic.js
+++ JAVASCRIPT/javascript_basic.js
@@ -3,21 +3,44 @@
 */
 
 // Number (int + float)
+
 // 따라서 / 도 int 범위에서 끝나지 않음 ** 로 거듭제곱 표현 가능
 // NaN는 Not a Number이지만 숫자로 침(0 / 0 등)
 
+
 // String
 
+// Have to use quote(", ') or backtick(`)
+// Strings are indexed(start from 0)
+// str.length > get to length of the str
+// str1 + str2 > concatenation
+
+// str.method();
+// toUpperCase, toLowerCase, trim
+// can chain the method str.method1().method2()
+
+// indexOf(arg) return the first occurent index of argument(if doesn't exist return -1)
+// slice(beginIndex[, endIndex]) //endIndex is optional
+
+// Templete literal
+// `I like ${3 + 4}` >> "I like 7"
+
+
 // Boolean(true or false)
+
 // use when performing logic
+
 
 // Null(typeof 시 object 반환 설계 결함으로 null은 기본형이 맞음)
 
+// primitive value
+// represent the intentional absence of any value
+
+
 // Undefined
 
-// Symbol
-
-// Bigint
+// primitive value
+// variables that do not have an assigned value are undefined
 
 /*
     Variables
@@ -45,3 +68,12 @@
 // snake case current_date(uncommon)
 // camel case currentDate(common)
 // (suggest) use is to show that it is a boolean(ex. isGameOver)
+
+/*
+    Math Object
+*/
+
+// Math.PI
+
+// Math.floor(), Math.ceil() >> round down, round up
+// Math.random() >> return random value(0 <= x < 1)
